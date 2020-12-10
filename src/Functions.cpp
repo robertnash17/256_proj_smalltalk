532dcc8f21ff17fd6d045f8f17eb98e36e5719f0
robertnash17 <robert.nash.17@cnu.edu>
2020-12-10 15:13:30 -0800

    did things

---
+++ src/Functions.cpp
@@ -0,0 +1,26 @@
+#include "../includes/Functions.h"
+
+/**
+ * create a vector with appropriate numbers of Smalltalk_Brit,Smalltalk_American and ST_American_DonutEnthusiest
+ * objects using pointers.
+ *
+ * \param mv	the vector
+ * \param numBrit  number of british smalltalkers
+ * \param numAmerican " AMerican "
+ * \param numbAmericanDonutEnthusiest " American Donut Enthusiest "
+ */
+void getPeople(std::vector<Smalltalk*> &mv,int numBrit = 1,
+		int numAmerican = 1, int numbAmericanDonutEnthusiest = 1){
+	return;
+}
+
+/**
+ * Iterate over the vector full of Smalltalk pointers, If a Smalltalk pointer is not null then
+ * delete what it points to.  Once all Smalltalk pointers deleted then use std::vector::clear()
+ * to clear the vector of the pointers.
+ *
+ * \param mv
+ */
+void clearVector(std::vector<Smalltalk*> &mv){
+	return;
+}
